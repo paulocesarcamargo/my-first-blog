10cf5fa266ebc475d63f19419e236035ff762cbc
Paulo Camargo <paulocesarcamargo@yahoo.com.br>
2021-01-02 12:17:40 -0300

    first commit

---
+++ crud/tasks/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+
+from django.http import HttpResponse
+
+from . models import *
+from . forms import *
+
+
+def index(request):
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect('/')
+
+    context = {'tasks':tasks, 'form':form}
+    
+    return render(request,'tasks/list.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    context = {'form':form}
+    return render(request,'tasks/update_task.html', context)
+
+
+
